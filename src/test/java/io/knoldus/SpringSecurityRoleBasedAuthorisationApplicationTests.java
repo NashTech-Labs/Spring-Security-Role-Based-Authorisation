813a4a2ce7a523e4b1facea2b69d3a3935da7d4d
Ankit Mogha <76722619+ankit-mogha@users.noreply.github.com>
2022-08-29 19:25:27 +0530

    Update and rename SpringBootApplicationWithSpringSecurityApplicationTests.java to SpringSecurityRoleBasedAuthorisationApplicationTests.java

--- src/test/java/io/knoldus/SpringSecurityRoleBasedAuthorisationApplicationTests.java
+++ src/test/java/io/knoldus/SpringSecurityRoleBasedAuthorisationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootApplicationWithSpringSecurityApplicationTests {
+class SpringSecurityRoleBasedAuthorisationApplicationTests {
 
 	@Test
 	void contextLoads() {
